81fe8521da908148b00f790dc93a06dca1743797
nicolasalp <nicolas_lopezutlook.com>
2023-01-18 16:36:56 -0300

    home page

--- add.php
+++ add.php
@@ -1,31 +1,29 @@
-<pre>
-
 <?php
+
   require "database.php";
 
   $error = null;
 
-  if ($_SERVER["REQUEST_METHOD"] == "POST"){
-      if (empty($_POST["name"]) || empty($_POST["phone_number"])){
-        $error = "Please fill all the fields";
-      } elseif(strlen($_POST["phone_number"] )<9){
-        $error = "Phone number must be at least 9 characters.";
-      }else{
-        $name = $_POST["name"];
-        $phoneNumber = $_POST["phone_number"];
-  
-        $statement = $conn->prepare("INSERT INTO contacts(name, phone_number) VALUES (:name, :phone_number)");
-        $statement->bindParam(":name", $_POST["name"]);
-        $statement->bindParam(":phone_number", $_POST["phone_number"]);
-        $statement->execute();
-  
-      header("Location: index.php");
-      }
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"]) || empty($_POST["phone_number"])) {
+      $error = "Please fill all the fields.";
+    } else if (strlen($_POST["phone_number"]) < 9) {
+      $error = "Phone number must be at least 9 characters.";
+    } else {
+      $name = $_POST["name"];
+      $phoneNumber = $_POST["phone_number"];
+
+      $statement = $conn->prepare("INSERT INTO contacts (name, phone_number) VALUES (:name, :phone_number)");
+      $statement->bindParam(":name", $_POST["name"]);
+      $statement->bindParam(":phone_number", $_POST["phone_number"]);
+      $statement->execute();
+
+      header("Location: home.php");
+    }
   }
 ?>
-</pre>
 
-<?php require "partial/header.php" ?>
+<?php require "partials/header.php" ?>
 
 <div class="container pt-5">
   <div class="row justify-content-center">
@@ -37,13 +35,13 @@
             <p class="text-danger">
               <?= $error ?>
             </p>
-          <?php endif?>
+          <?php endif ?>
           <form method="POST" action="add.php">
             <div class="mb-3 row">
               <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
 
               <div class="col-md-6">
-                <input id="name" type="text" class="form-control" name="name"  autocomplete="name" autofocus>
+                <input id="name" type="text" class="form-control" name="name" autocomplete="name" autofocus>
               </div>
             </div>
 
@@ -51,7 +49,7 @@
               <label for="phone_number" class="col-md-4 col-form-label text-md-end">Phone Number</label>
 
               <div class="col-md-6">
-                <input id="phone_number" type="tel" class="form-control" name="phone_number"  autocomplete="phone_number" autofocus>
+                <input id="phone_number" type="tel" class="form-control" name="phone_number" autocomplete="phone_number" autofocus>
               </div>
             </div>
 
@@ -66,4 +64,5 @@
     </div>
   </div>
 </div>
-<?php require "partial/footer.php" ?>
+
+<?php require "partials/footer.php" ?>
