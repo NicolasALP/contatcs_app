010b310c43c3c5a2853fc7df17f79d1981ad0cb1
nicolasalp <nicolas_lopezutlook.com>
2023-01-19 01:55:30 -0300

    Create user

--- sql/setup.sql
+++ sql/setup.sql
@@ -5,6 +5,15 @@ CREATE DATABASE contacts_app;
 
 USE contacts_app;
 
+CREATE TABLE users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255)
+);
+
+insert into users (name,email,password) values ("test", "test@test.com", "123");
+
 CREATE TABLE contacts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
